f96df9ab19d4b817a2a2cf15fbc82edd3a51abeb
Piotr <p.skolorzynski@gmail.com>
2021-08-26 23:02:55 +0200

    modify List.js render people from data.js modify App.js pass people from data.js to component List

--- src/List.js
+++ src/List.js
@@ -1,7 +1,18 @@
-const List = () => {
+const List = ({people}) => {
     return (
         <>
-            <h2>list component</h2>
+            {people.map(person => {
+                const {id, name, age, image} = person;
+                return (
+                    <section className="person" key={id}>
+                        <img src={image} alt={name} />
+                        <div>
+                            <h4>{name}</h4>
+                            <p>{age}</p>
+                        </div>
+                    </section>
+                )
+            })}
         </>
     );
 }
